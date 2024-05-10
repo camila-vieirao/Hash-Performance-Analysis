a55d8c03f87880aa05bfcb39afffc5770eecc724
Camila Vieira de Oliveira <127426979+camila-vieirao@users.noreply.github.com>
2024-05-10 00:01:31 -0300

    feat: autenticação de usuário

--- project/autenticacao.py
+++ project/autenticacao.py
@@ -1,59 +1,73 @@
-import csv
-
 def menu_inicial():
-    print("Bem vindo!")
-    print("Digite uma opção para começar:")
-    print("  1. Cadastrar")
-    print("  2. Autenticar")
-    print("  3. Sair")
 
-    op = input("Opção:")
-    
-    #cadastrar
-    if op == "1":
-        usuario = input("Usuário: ")
-        senha = input("Senha: ")
+    while True:
+        print("Bem vindo!")
+        print("Digite uma opção para começar:")
+        print("  1. Cadastrar")
+        print("  2. Autenticar")
+        print("  3. Sair")
 
-    #autenticar
-    if op == "2":
-        cadastra()
-         
-    #sair
-    if op == "3":
-        print("Tchau! :]") 
+        op = input("\nOpção:")
+        
+        #cadastrar
+        if op == "1":
+            usuario = input("\nCrie um usuário: ")
+            senha = input("Crie uma senha: ")
+            if len(senha) > 4 or len(usuario) > 4:
+                print("\nAtenção: as credenciais devem ter no máximo 4 caracteres!\n")
+            else:
+                if cadastra(usuario, senha):
+                    print("\nNovo usuário cadastrado!\n")
+                else:
+                    print("Usuário já existe! :(\n")
 
-def cadastra():
-    with open("usuarios.txt", "r") as usuarios:
-        print("Atenção: as credenciais devem ter no máximo 4 caracteres!")
-        usuario = input("Crie um usuário: ")
-        senha = input("Crie uma senha: ")
+        #autenticar
+        elif op == "2":
+            usuario = input("Usuário: ")
+            senha = input("Senha: ")
+
+            auth = autentica(usuario, senha)
+
+            if auth:
+                print("\nUsuário autenticado!")
+                print("Olá, " + usuario)
+                return usuario
+            elif auth == None:
+                print("Usuário não existe! :c")
+            else:
+                print("Falha na autenticação! :c")
+            
+        #sair
+        elif op == "3":
+            print("Tchau! :]")
+            break
+
+        else: 
+            print("Opção não existe! :c")
+
+def cadastra(usuario, senha):
+
+    with open("project/usuarios.txt", "r") as usuarios:
 
         d = []
         f = []
         for i in usuarios:
-            a, b = i.split(", ")
+            a,b = i.split(", ")
             b = b.strip()
             d.append(a)
             f.append(b)
-        credenciais = dict(zip(d,f))
 
-        if len(senha or usuario)>4:
-            print("Atenção: as credenciais devem ter no máximo 4 caracteres!")
-            cadastra()
-        elif usuario in d:
-                print("Usuário já existe! :(")
-                cadastra()
-        else: 
-            with open("usuarios.txt", "a") as usuarios:        
-                usuarios.write(usuario + ", " + senha + "\n")
-                print("Novo usuário cadastrado!")
+
+    if usuario in d:
+        return False
+    else: 
+        with open("project/usuarios.txt", "a") as usuarios:        
+            usuarios.write(usuario + ", " + senha + "\n")
+    return True
         
-def autentica():
+def autentica(usuario, senha):
 
-    with open("usuarios.txt", "r") as usuarios:
-
-        usuario = input("Usuário: ")
-        senha = input("Senha: ")
+    with open("project/usuarios.txt", "r") as usuarios:
 
         if not len(usuario or senha)<1:
             d = []
@@ -63,4 +77,66 @@ def autentica():
                 b = b.strip()
                 d.append(a)
                 f.append(b)
-            credenciais = dict(zip(d,f))
+
+
+        if usuario not in d:
+            return None
+        
+        i = d.index(usuario)
+
+        if f[i] != senha:
+            return False
+        
+        return True
+
+def comandos_disponiveis(usuario):
+    while True:
+        print("\nComandos disponíveis:")
+        print("  1. Listar arquivos")
+        print("  2. Criar arquivo")
+        print("  3. Ler arquivo")
+        print("  4. Excluir arquivo")
+        print("  5. Executar arquivo")
+        print("  6. Sair")
+
+        op = input("\nOpção: ")    
+
+
+        # 1- Listar arquivos
+
+        if op == "1":
+            lista_arquivo()
+
+
+        # 2- Criar arquivo
+
+        elif op == "2":
+            cria_arquivo()
+
+
+        # 3- Ler arquivo
+
+        elif op == "3":
+            le_arquivo()
+
+
+        # 4- Excluir arquivo
+
+        elif op == "4":
+            exclui_arquivo()
+
+
+        # 5- Executar arquivo
+
+        elif op == "5":
+            executa_arquivo()
+
+
+        # 6- Sair
+
+        elif op == "6":
+            print("Tchau! :]")
+            break
+
+        else:
+            print("Opção não existe! :c")    
---
+++ project/main.py
@@ -0,0 +1,5 @@
+from autenticacao import menu_inicial
+from autenticacao import comandos_disponiveis
+
+if __name__ == '__main__':
+    usuario = menu_inicial()
